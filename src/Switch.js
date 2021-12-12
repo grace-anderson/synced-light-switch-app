37cf21f091760d00cd74d7c5a33f5c6ebf0efe1a
Helen G Anderson <grace.anderson.au@gmail.com>
2021-12-12 14:21:10 +1100

    Initial commit

---
+++ src/Switch.js
@@ -0,0 +1,14 @@
+//import React, { Component } from "react";
+import React from "react";
+
+const Switch = (props) => (
+  <div className="switch-plate">
+    <div className="screw" />
+    <div className="switch" onClick={props.toggle}>
+      <div className="switch-handle" />
+    </div>
+    <div className="screw" />
+  </div>
+);
+
+export default Switch;
